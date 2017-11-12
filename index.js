8406da123de0a7dbc7df6837eaf6e36c3b448fae
Zhan Peng Pan <zhanpenp@usc.edu>
2017-11-11 22:35:14 -0800

    major change

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
 
-var uri = "127.0.0.1:27017";
-//var uri = "mongodb://intermsof:ApsSDZFiblNXpi47@cluster0-shard-00-00-vuqjp.mongodb.net:27017,cluster0-shard-00-01-vuqjp.mongodb.net:27017,cluster0-shard-00-02-vuqjp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
+//var uri = "127.0.0.1:27017";
+var uri = "mongodb://intermsof:ApsSDZFiblNXpi47@cluster0-shard-00-00-vuqjp.mongodb.net:27017,cluster0-shard-00-01-vuqjp.mongodb.net:27017,cluster0-shard-00-02-vuqjp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
 var db = mongoose.connect(uri);
 var app = express();
 var port = process.env.PORT || 3000;
